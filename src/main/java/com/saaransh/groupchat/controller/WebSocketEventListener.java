7b07c091b2ae4da7b34c47d07d52f81403431c0a
Saaransh Shaw <saaransh_shaw@apple.com>
2022-01-20 12:59:32 +0530

    Initial commit

---
+++ src/main/java/com/saaransh/groupchat/controller/WebSocketEventListener.java
@@ -0,0 +1,44 @@
+package com.saaransh.groupchat.controller;
+
+import com.saaransh.groupchat.model.ChatMessage;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.logging.Logger;
+
+@Component
+public class WebSocketEventListener {
+
+    private static final Logger logger = Logger.getLogger("");
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListeners(SessionConnectedEvent event) {
+        logger.info("Recieved a new web socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            logger.info("User Disconnected : " + username);
+
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setSender(username);
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+
+}
---
+++ src/test/java/com/saaransh/groupchat/GroupChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saaransh.groupchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
